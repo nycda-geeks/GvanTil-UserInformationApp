9c7edf2d7ef9dae54971f9c16ea135ab6cc63cc9
Gijs van Til <gijsvantil@gmail.com>
2016-05-10 17:10:49 +0200

    started with the building of the get-part in app.js. Added fileread

--- src/app.js
+++ src/app.js
@@ -6,3 +6,13 @@ var app = express();
 app.set('views', '.src/views');
 app.set('view engine', 'jade');
 
+app.get ('/', function (request, response){
+	fs.readFile('../resources/users.json', function (error, data){
+		if (error){
+			console.log ("Apparently something went wrong: " + error)
+		}
+
+		
+
+	});
+});
