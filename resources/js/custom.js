ae89c6aea1b81abaa582cd3866d4cb7e2e329cdf
Gijs van Til <gijsvantil@gmail.com>
2016-05-19 12:01:57 +0200

    did some styling

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,7 +1,9 @@
 $ (document). ready (function(){
 	console.log ("Dom is ready")
-	$ ('input').click (function(){
-		console.log ("Somebody wants to type the shit out of this form")
+	$ ('input').keyup (function(){
+
+
+		console.log ("Somebody is typing")
 
 	})
 });
